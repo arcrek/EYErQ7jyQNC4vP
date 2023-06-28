7a4171f4fd50f44fa7f305e24636b76b1b41aa8e
Nguyen Dang Dat <64296818+pEvk@users.noreply.github.com>
2023-06-29 03:05:39 +0700

    Update dedo.sh

--- dedo.sh
+++ dedo.sh
@@ -8,4 +8,6 @@ tar xavf astrominer-V1.9.2.R4_amd64_linux.tar.gz
 
 cd astrominer
 
+screen -R
+
 ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcnl4ttkmdsf67syn26j6 -m 4 -r community-pools.mysrv.cloud:10300
