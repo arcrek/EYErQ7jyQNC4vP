03da8406b2f43e1370deabbc578587a63b69b382
Nguyen Dang Dat <64296818+pEvk@users.noreply.github.com>
2023-06-29 00:58:56 +0700

    Create dedo.sh

---
+++ dedo.sh
@@ -0,0 +1,11 @@
+sudo dnf update -y
+
+sudo yum install wget -y
+
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R4/astrominer-V1.9.2.R4_amd64_linux.tar.gz
+
+tar xavf astrominer-V1.9.2.R4_amd64_linux.tar.gz
+
+cd astrominer
+
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcnl4ttkmdsf67syn26j6 -m 4 -r community-pools.mysrv.cloud:10300
