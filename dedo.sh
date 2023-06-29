6fe76242df2eb2f2c683611c847e24ee7dbf2b0c
Nguyen Dang Dat <64296818+pEvk@users.noreply.github.com>
2023-06-29 16:32:00 +0700

    Update dedo.sh

--- dedo.sh
+++ dedo.sh
@@ -1,11 +1,41 @@
-sudo dnf update -y
+#!/bin/sh
 
-sudo yum install wget -y
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R5/astrominer-V1.9.2.R5_amd64_linux.tar.gz
 
-wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R4/astrominer-V1.9.2.R4_amd64_linux.tar.gz
+tar -xf astrominer-V1.9.2.R5_amd64_linux.tar.gz
 
-tar xavf astrominer-V1.9.2.R4_amd64_linux.tar.gz
+sudo cp astrominer /usr/bin
 
-cd astrominer
+sudo touch  /etc/systemd/system/astrominer.service
 
-./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcnl4ttkmdsf67syn26j6 -m 4 -r community-pools.mysrv.cloud:10300
+sudo chmod 666 /etc/systemd/system/astrominer.service
+
+cat << EOF > /etc/systemd/system/astrominer.service 
+ 
+[Unit] 
+ 
+Description=astrominer 
+ 
+After=network.target 
+ 
+StartLimitIntervalSec=0 
+ 
+[Service] 
+ 
+Type=simple 
+ 
+Restart=always 
+ 
+RestartSec=3
+ 
+ExecStart=/usr/bin/astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcnl4ttkmdsf67syn26j6 -r dero-node-overlode.mysrv.cloud:10300 -r1 community-pools.mysrv.cloud:10300 -p rpc
+ 
+[Install] 
+ 
+WantedBy=multi-user.target 
+ 
+EOF
+sudo chmod 600 /etc/systemd/system/astrominer.service
+sudo systemctl enable astrominer
+sudo systemctl daemon-reload
+sudo systemctl restart astrominer
